a4245b490aef5aa579e56f370d569cb8a2cb205c
Vladimir Nechai <vne@adorsys.de>
2019-05-22 15:59:29 +0200

    special error handling for login pages

--- oba-ui/src/app/common/interceptors/ObaErrorsHandler.ts
+++ oba-ui/src/app/common/interceptors/ObaErrorsHandler.ts
@@ -1,9 +1,14 @@
 import {ErrorHandler, Injectable, Injector, NgZone} from "@angular/core";
 import {ActivatedRoute, Router} from "@angular/router";
 import {InfoService} from "../info/info.service";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Injectable()
 export class ObaErrorsHandler implements ErrorHandler {
+  private encryptedConsentId: string;
+  private paymentId: string;
+  private redirectId: string;
+
   constructor(
     private zone: NgZone,
     private infoService: InfoService,
@@ -18,34 +23,61 @@ export class ObaErrorsHandler implements ErrorHandler {
     return this.injector.get(ActivatedRoute);
   }
 
-  public handleError(error: any) {
+  public handleError(error: HttpErrorResponse) {
     console.error(error);
-    let httpErrorCode = error.httpErrorCode;
 
-    this.activatedRoute.queryParams.subscribe(params => {
-      console.log(params);
-    });
+    let httpErrorCode = error.status;
 
-    switch(httpErrorCode) {
+    // if router is on the login page: special error handling
+    const login = this.router.url.indexOf('/login') > 0;
+    if (login) {
+
+      this.activatedRoute.queryParams.subscribe(params => {
+        this.encryptedConsentId = params['encryptedConsentId'];
+        this.paymentId = params['paymentId'];
+        this.redirectId = params['redirectId'];
+      });
+
+      // AIS: if no redirectId or encryptedConsentId is provided
+      if (this.encryptedConsentId === undefined || this.redirectId === undefined && (this.router.url.indexOf('/account-information') > 0) ) {
+        this.infoService.openFeedback('No consent data is provided', {
+          severity: 'error'
+        });
+        return;
+      }
+
+      // PIS and Payment Cancellation: if no redirectId or paymentId is provided
+      if (this.paymentId === undefined || this.redirectId === undefined && !(this.router.url.indexOf('/account-information') > 0) ) {
+        this.infoService.openFeedback('No payment data is provided', {
+          severity: 'error'
+        });
+        return;
+      }
+    }
+
+    // default error handling
+    switch (httpErrorCode) {
       case 401: {
-        this.infoService.openFeedback('401 Error occured', {
+        this.infoService.openFeedback('Invalid credentials', {
           severity: 'error'
         });
         break;
       }
       case 403: {
-        this.infoService.openFeedback('403 Error occured', {
+        this.infoService.openFeedback('You have no rights to execute this action', {
           severity: 'error'
         });
         break;
       }
       default: {
-        this.infoService.openFeedback('500 Error occured', {
+        // if required could be redirected to internal server error page
+        this.infoService.openFeedback('Internal server error. Please contact technical support', {
           severity: 'error'
         });
         break;
       }
     }
+
   }
 
 }
