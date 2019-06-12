b318ab4974c09312930bae0d851efd87e844e213
Yurii Averin <averin.yury@gmail.com>
2019-06-12 15:45:23 +0300

    Fix headers init.

--- developer-portal-ui/src/pages/test-cases/components/api-endpoints/accinf-transactions-get/accinf-transactions-get.component.ts
+++ developer-portal-ui/src/pages/test-cases/components/api-endpoints/accinf-transactions-get/accinf-transactions-get.component.ts
@@ -7,20 +7,13 @@ import { Component, OnInit } from '@angular/core';
 export class AccinfTransactionsGetComponent implements OnInit {
 
   activeSegment = 'documentation';
-  headers: object = {};
-  body: object = {};
+  headers: object = {
+    'X-Request-ID': '2f77a125-aa7a-45c0-b414-cea25a116035',
+    'Consent-ID': 'CONSENT_ID',
+    'PSU-IP-Address': '1.1.1.1',
+  };
 
-  constructor() {
-    this.init();
-  }
-
-  init() {
-    this.headers = {
-      'X-Request-ID': '2f77a125-aa7a-45c0-b414-cea25a116035',
-      'Consent-ID': 'CONSENT_ID',
-      'PSU-IP-Address': '1.1.1.1',
-    };
-  }
+  constructor() {}
 
   changeSegment(segment) {
     this.activeSegment = segment;
