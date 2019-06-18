8b0546e5f7681f28988da1e9111216a0ecce4e84
Petro Rudenko <pru@adorsys.com.ua>
2019-06-18 17:29:34 +0300

    remove obsolete class

--- tpp-app/tpp-rest-server/src/main/java/de/adorsys/psd2/sandbox/tpp/rest/server/service/DataUploadConfiguration.java
+++
@@ -1,9 +0,0 @@
-package de.adorsys.psd2.sandbox.tpp.rest.server.service;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("de.adorsys.psd2.sandbox.tpp.rest.server")
-public class DataUploadConfiguration {
-}
