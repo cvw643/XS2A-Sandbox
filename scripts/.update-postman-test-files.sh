5f39ab444ecee735a2a9a91165e55a2fae38ee3a
Tarkan Nein <tne@adorsys.de>
2019-07-12 13:02:06 +0000

    fix typo

--- scripts/.update-postman-test-files.sh
+++ scripts/.update-postman-test-files.sh
@@ -2,7 +2,7 @@
 #Remove oldest one files from developer-portal.
 #Add new files
 set -e
-echo "Delete previous zip files"
+echo "Delete previous zip file"
 rm -f developer-portal-ui/src/assets/files/postman-tests.zip
 zip -j developer-portal-ui/src/assets/files/postman-tests.zip postman-scripts/postman_collection.json postman-scripts/postman_environment_local.json postman-scripts/postman_global_variable.json
 
